29ba87196fdda46e09288bc32506f7cd25a7556f
Barbiche <guethomas3@gmail.com>
2016-04-25 15:21:04 +0200

    Début de l'application MainActivity, et Map pour parcours, non liées

---
+++ AppSmartooz/app/src/main/java/aieapp/appsmartooz/Parcours.java
@@ -0,0 +1,34 @@
+package aieapp.appsmartooz;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Parcours extends FragmentActivity
+        implements OnMapReadyCallback {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parcours);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+
+    }
+}
